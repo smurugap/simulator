4bbf2402f607f3be1c6b7ec723e3ee1768de8461
Senthilnathan Murugappan <msenthil@juniper.net>
2020-08-07 15:53:33 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 mkdir -p /etc/simulator
+echo 1024 > /proc/sys/fs/inotify/max_user_instances
 name=${2:-simulator-agent}
 image=${1:-smurugap/simulator:latest}
 docker run -itd --privileged -v /var/run:/var/run -v /etc/simulator:/etc/simulator --net host --name $name $image
