f5a31e6ea28a6b0035662b0943a570dcd46ffd62
Senthilnathan Murugappan <msenthil@juniper.net>
2020-11-17 12:51:06 -0800

    Adding spyne patches

--- docker/apply_patches.sh
+++ docker/apply_patches.sh
@@ -3,4 +3,8 @@
 cd /usr/lib/python2.7/site-packages/netconf
 git apply /patches/netconf.patch.1
 
+# Apply spyne patch
+cd /usr/lib/python2.7/site-packages/spyne
+git apply /patches/spyne.patch.1
+
 # Apply exabgp patch
